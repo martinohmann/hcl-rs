fcca74861d1d6e9ccbdf1e50de4b8b24de7b5b80
martinohmann <martinohmann@gmail.com>
2023-02-02 07:22:56 +0100

    avoid re-parsing structure identifiers

--- src/parser/nom/spanned/structure.rs
+++ src/parser/nom/spanned/structure.rs
@@ -1,70 +1,57 @@
-use super::ast::{Attribute, Block, Body, Structure};
-use super::{char_or_cut, expr::expr, ident, line_comment, sp, string, ws, IResult};
-use super::{decorated, prefix_decorated, spanned, suffix_decorated, Node, Span};
+use super::ast::{Attribute, Block, Body, Expression, Structure};
+use super::{
+    char_or_cut, decorated, expr::expr, ident, line_comment, prefix_decorated, sp, spanned, string,
+    suffix_decorated, ws, IResult, Node, Span,
+};
 use crate::structure::BlockLabel;
 use nom::{
     branch::alt,
-    character::complete::{char, line_ending},
-    combinator::{cut, eof, map, opt, value},
+    character::complete::{anychar, char, line_ending},
+    combinator::{cut, eof, map, opt, peek, value},
     multi::many0,
-    sequence::{delimited, preceded, separated_pair, terminated, tuple},
+    sequence::{delimited, pair, preceded, terminated},
 };
 
-fn line_ending_terminated<'a, F, O>(inner: F) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, O>
-where
-    F: FnMut(Span<'a>) -> IResult<Span<'a>, O>,
-{
-    terminated(
-        inner,
-        preceded(
-            sp,
-            alt((value((), line_ending), value((), eof), line_comment)),
-        ),
-    )
-}
-
-fn attribute(input: Span) -> IResult<Span, Attribute> {
-    map(
-        separated_pair(
-            suffix_decorated(ident, sp),
-            char('='),
-            cut(prefix_decorated(sp, expr)),
-        ),
-        |(key, expr)| Attribute { key, expr },
+fn line_trailing(input: Span) -> IResult<Span, ()> {
+    preceded(
+        sp,
+        alt((value((), line_ending), value((), eof), line_comment)),
     )(input)
 }
 
-fn block(input: Span) -> IResult<Span, Block> {
-    map(
-        tuple((
-            suffix_decorated(ident, sp),
-            many0(suffix_decorated(block_label, sp)),
-            alt((
-                // Multiline block.
-                delimited(
-                    line_ending_terminated(char_or_cut('{')),
-                    body,
-                    char_or_cut('}'),
-                ),
-                // One-line block.
-                spanned(map(
-                    delimited(
-                        char_or_cut('{'),
-                        opt(cut(decorated(sp, map(attribute, Structure::Attribute), sp))),
-                        char_or_cut('}'),
-                    ),
-                    |attr| Body {
-                        structures: attr.map(|attr| vec![attr]).unwrap_or_default(),
-                    },
-                )),
+fn attribute_expr(input: Span) -> IResult<Span, Node<Expression>> {
+    preceded(char('='), prefix_decorated(sp, cut(expr)))(input)
+}
+
+fn block_body(input: Span) -> IResult<Span, Node<Body>> {
+    let single_attribute = map(
+        pair(suffix_decorated(ident, sp), attribute_expr),
+        |(key, expr)| Structure::Attribute(Attribute { key, expr }),
+    );
+
+    delimited(
+        char_or_cut('{'),
+        alt((
+            // Multiline block.
+            preceded(line_trailing, body),
+            // One-line block.
+            spanned(map(
+                opt(decorated(sp, cut(single_attribute), sp)),
+                |structure| Body {
+                    structures: structure.map(|s| vec![s]).unwrap_or_default(),
+                },
             )),
         )),
-        |(identifier, labels, body)| Block {
-            identifier,
-            labels,
-            body,
-        },
+        char_or_cut('}'),
     )(input)
+}
+
+fn block_labels(input: Span) -> IResult<Span, Vec<Node<BlockLabel>>> {
+    many0(suffix_decorated(block_label, sp))(input)
+}
+
+fn block_parts(input: Span) -> IResult<Span, (Vec<Node<BlockLabel>>, Node<Body>)> {
+    pair(block_labels, block_body)(input)
 }
 
 fn block_label(input: Span) -> IResult<Span, BlockLabel> {
@@ -75,16 +62,29 @@ fn block_label(input: Span) -> IResult<Span, BlockLabel> {
 }
 
 fn structure(input: Span) -> IResult<Span, Structure> {
-    alt((
-        map(attribute, Structure::Attribute),
-        map(block, Structure::Block),
-    ))(input)
+    let (input, ident) = suffix_decorated(ident, sp)(input)?;
+    let (input, ch) = peek(anychar)(input)?;
+
+    if ch == '=' {
+        let (input, expr) = attribute_expr(input)?;
+        Ok((input, Structure::Attribute(Attribute { key: ident, expr })))
+    } else {
+        let (input, (labels, body)) = block_parts(input)?;
+        Ok((
+            input,
+            Structure::Block(Block {
+                identifier: ident,
+                labels,
+                body,
+            }),
+        ))
+    }
 }
 
 pub fn body(input: Span) -> IResult<Span, Node<Body>> {
     suffix_decorated(
         map(
-            many0(prefix_decorated(ws, line_ending_terminated(structure))),
+            many0(prefix_decorated(ws, terminated(structure, line_trailing))),
             |structures| Body { structures },
         ),
         ws,
